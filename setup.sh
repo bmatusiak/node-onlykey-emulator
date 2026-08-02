19eb7dd69302bb1ff9afbcfc93af756a98404544
bmatusiak <bmatusiak@gmail.com>
2026-08-02 12:21:42 -0400

    Add setup script for initializing OnlyKey emulator environment

---
+++ setup.sh
@@ -0,0 +1,14 @@
+
+
+mkdir onlykey
+cd onlykey
+git clone https://github.com/bm-ok/0c-coder-libraries ./libraries
+git clone https://github.com/bm-ok/OnlyKey-Firmware
+
+
+cd ../ui
+npm install
+
+cd ../
+npm install
+
